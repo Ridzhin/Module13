02e7d7ea199ef468ef6351aeb867379a6deee695
Ridzhin <126336074+Ridzhin@users.noreply.github.com>
2023-05-26 15:06:06 +0300

    Add files via upload

---
+++ Module13/Task1/Task1.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+internal class Task1
+ {
+     static void Main(string[] args)
+        {
+            var WatchOne = Stopwatch.StartNew();
+            var strings = new List<string>();
+            string text = File.ReadAllText("C:\\Users\\ridzh\\Documents\\Text1.txt");
+            strings.Add(text);
+            Console.WriteLine($"Вставка в List: {WatchOne.Elapsed.TotalMilliseconds} мс");
+           
+            var WatchTwo = Stopwatch.StartNew();
+            LinkedList<string> LinkedList = new LinkedList<string>();
+            text = File.ReadAllText("C:\\Users\\ridzh\\Documents\\Text1.txt");
+            LinkedList.AddFirst(text);
+            Console.WriteLine($"Вставка в LinkedList: {WatchTwo.Elapsed.TotalMilliseconds} мс");
+        }
+}
---
+++ Module13/Task2/Task2.cs
@@ -0,0 +1,40 @@
+﻿   internal class Task2
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText("C:\\Users\\ridzh\\Documents\\Text1.txt"); 
+            var noPunctualitenText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+            char[] nocount = { ' ', '\r', '\t' }; 
+            var words = noPunctualitenText.Split(nocount, StringSplitOptions.RemoveEmptyEntries);
+            var RepWords = new Dictionary<string, int>();
+            int show = 0;
+
+            foreach (var word in words)
+            {
+                bool rep = RepWords.ContainsKey(word); 
+                int count;
+
+                if (rep is true)  
+                {
+                    RepWords.TryGetValue(word, out count);
+                    RepWords.Remove(word);
+                    RepWords.Add(word, count + 1);
+                }
+
+                else
+                {
+                    RepWords.Add(word, 1);
+                }
+            }
+
+            foreach (KeyValuePair<string, int> word in RepWords.OrderByDescending(key => key.Value))
+            {
+                if (show < 10)
+                {
+                    Console.WriteLine($"Слово {word.Key} упоминалось {word.Value} раз");
+                    show++;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
